bca61b02db9cf3ff321619eac8b0f2b878de74ae
stevengerdes <stevengerdes@outlook.com>
2016-08-15 20:00:50 -0600

    Added the Functions file

---
+++ Functions.py
@@ -0,0 +1,8 @@
+def PercentOfTotal(aList):
+    sumOfElements = 0
+    for element in aList:
+        sumOfElements += element
+    return aList/sumOfElements #divides every element by the sum
+
+
+
